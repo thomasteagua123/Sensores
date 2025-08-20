146023a15e483f8c5f3855b0f841c333ac65f248
luchovs <lucianosavia15@gmail.com>
2025-08-20 09:46:48 -0300

    Acceso a la base de datos

--- servidor/app.py
+++ servidor/app.py
@@ -1,5 +1,28 @@
-from flask import Flask, request
+import sqlite3
+from flask import Flask, g, request
+
+
+def dict_factory(cursor, row):
+ """Arma un diccionario con los valores de la fila."""
+ fields = [column[0] for column in cursor.description]
+ return {key: value for key, value in zip(fields, row)}
+
+
+def abrirConexion():
+  if 'db' not in g:
+     g.db = sqlite3.connect("sensores.sqlite")
+     g.db.row_factory = dict_factory
+  return g.db
+
+
+def cerrarConexion(e=None):
+   db = g.pop('db', None)
+   if db is not None:
+       db.close()
+
 app = Flask(__name__)
+app.teardown_appcontext(cerrarConexion)
+resultados_por_pag = 10
 
 @app.route("/")
 def hello_world():
@@ -8,7 +31,9 @@ def hello_world():
 @app.route("/api/sensor/", methods=['POST'])
 def sensor():
     datos = request.json
+    db = abrirConexion()
     sensor=datos["nombre"]
     valor=datos["valor"]
     print(f"Sensor: ",sensor, "valor:" ,valor,)
+    cerrarConexion()
     return "OK"
